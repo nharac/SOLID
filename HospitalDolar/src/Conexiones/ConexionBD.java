9d6d4ffc0024e9c3b675d74db0040d1f21a9c1b1
Nhara <estefacala03@hotmail.com>
2026-02-16 20:50:19 -0500

    Base de datos conectada

--- HospitalDolar/src/Conexiones/ConexionBD.java
+++ HospitalDolar/src/Conexiones/ConexionBD.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Conexiones;
 
-/**
- *
- * @author ESTUDIANTE
- */
+package Conexiones;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+
+    private static final String URL ="jdbc:mysql://localhost:3306/dolar_association?useSSL=false&serverTimezone=UTC";
+    private static final String Usuario ="root";
+    private static final String Contrasena="";
     
+    public static Connection getConexion (){
+     try {
+         Connection conexion =DriverManager.getConnection(URL,Usuario,Contrasena);
+         System.out.println("Ingreso exitoso");
+         return conexion;
+     }
+     catch(SQLException error){
+         System.out.println("Conexion fallida");
+         error.printStackTrace();
+         return null;
+     }
+        }
+    public static void main(String[] args) {
+        getConexion();
+    }
 }
