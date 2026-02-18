47d094830038d4db4e7b9fc675abcfbfe8aa93f2
Camila <lchavesac.01@gmail.com>
2026-02-18 09:39:08 -0500

    Base de datos añadida

--- dolar_association.sql
+++ dolar_association.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-02-2026 a las 19:19:45
+-- Tiempo de generación: 18-02-2026 a las 15:38:31
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -29,21 +29,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `medicamentos` (
   `id` int(100) NOT NULL,
-  `descripcion` varchar(200) NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
   `nombre` text NOT NULL,
-  `cantidad` int(100) NOT NULL
+  `cantidad` int(100) NOT NULL,
+  `Estado` varchar(10) NOT NULL DEFAULT 'Activo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `medicamentos`
 --
 
-INSERT INTO `medicamentos` (`id`, `descripcion`, `nombre`, `cantidad`) VALUES
-(1, 'Dolores leves como cabeza y muscular.', 'Ibuprofeno', 10),
-(2, 'Medicamento indicado principalmente en los casos de hipertensión e insuficiencia cardíaca, ya que actúa promoviendo la dilatación de los vasos sanguíneos, evitando derrames cerebrales', 'Losartan', 0),
-(3, 'Medicamento antihipertensivo que se utiliza para tratar la presión arterial alta y la angina de pecho. Reduce la carga del corazón.', 'Amlodipino', 0),
-(4, 'Anticonceptivo hormonal oral combinado que se utiliza para prevenir el embarazo. Se indica para tratar condiciones como la dismenorrea, endometriosis, y crisis ovulatorias.', 'Microgynon', 0),
-(5, 'Medicamento utilizado para reducir la fiebre y aliviar el dolor. Se utiliza comúnmente para tratar dolores leves a moderados, como dolores de cabeza, dolor de garganta y fiebre.', 'Acetaminofén', 0);
+INSERT INTO `medicamentos` (`id`, `descripcion`, `nombre`, `cantidad`, `Estado`) VALUES
+(1, 'Dolores leves como cabeza y muscular.', 'Ibuprofeno', 10, 'Activo'),
+(2, 'Medicamento indicado principalmente en los casos de hipertensión e insuficiencia cardíaca, ya que actúa promoviendo la dilatación de los vasos sanguíneos, evitando derrames cerebrales', 'Losartan', 0, 'Activo'),
+(3, 'Medicamento antihipertensivo que se utiliza para tratar la presión arterial alta y la angina de pecho. Reduce la carga del corazón.', 'Amlodipino', 0, 'Activo'),
+(4, 'Anticonceptivo hormonal oral combinado que se utiliza para prevenir el embarazo. Se indica para tratar condiciones como la dismenorrea, endometriosis, y crisis ovulatorias.', 'Microgynon', 0, 'Activo'),
+(5, 'Medicamento utilizado para reducir la fiebre y aliviar el dolor. Se utiliza comúnmente para tratar dolores leves a moderados, como dolores de cabeza, dolor de garganta y fiebre.', 'Acetaminofén', 0, 'Inactivo'),
+(9, 'adjhjksd', 'jkhaskdhaksdj', 20, 'Activo'),
+(10, 'Pachito', '', 14, 'Activo');
 
 --
 -- Índices para tablas volcadas
@@ -63,7 +66,7 @@ ALTER TABLE `medicamentos`
 -- AUTO_INCREMENT de la tabla `medicamentos`
 --
 ALTER TABLE `medicamentos`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
